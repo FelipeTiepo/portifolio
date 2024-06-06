132d1ebc513fa97721ba51fdf796fc089426a1c7
Felipe Tiepo <ftiepo20@gmail.com>
2024-06-06 19:47:00 -0300

    adiconando a transição entre as pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 //components
 import Header from "@/components/Header"
+import PageTrasition from "@/components/PageTrasition";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], 
 weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
@@ -18,7 +19,8 @@ export default function RootLayout({children}) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
-        {children}</body>
+        <PageTrasition>{children}</PageTrasition>
+        </body>
     </html>
   );
 }
