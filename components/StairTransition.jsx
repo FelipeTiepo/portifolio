8b33f5c17c74f31d1218ca866b723ba1c3e1f927
Felipe Tiepo <ftiepo20@gmail.com>
2024-06-10 15:31:08 -0300

    adicionando o menu de navegação para celular

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 //components
@@ -10,15 +10,24 @@ const StairTransition = () => {
     const pathname = usePathname();
     return (
         <>
-        <AnimatePresence mode="wait">
-            <div key={pathname}>
-                <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex ">
-                    <Stairs />
+            <AnimatePresence mode="wait">
+                <div key={pathname}>
+                    <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex ">
+                        <Stairs />
+                    </div>
+
+                    <motion.div
+                        className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+                        initial={{ opacity: 1 }}
+                        animate={{
+                            opacity: 0,
+                            transition: { delay: 1, duration: 0.2, ease: "easeInOut" }
+                        }}
+                    />
                 </div>
-            </div>
-        </AnimatePresence>
+            </AnimatePresence>
         </>
-        );
+    );
 };
 
 export default StairTransition;
