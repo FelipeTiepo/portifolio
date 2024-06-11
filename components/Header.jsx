c356c50999903e6e6c6a8107fcedff65542dfecc
Felipe Tiepo <ftiepo20@gmail.com>
2024-06-10 21:29:22 -0300

    fazendo a pagina inicial e adicionando as fotos

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         {/* logo */}
         <Link href="/">
             <h1 className="text-4xl font-semibold">
-                Felipe Tiepo<span className="text-accent">.</span>
+                Felipe<span className="text-accent">.</span>
             </h1>
         </Link>
 
